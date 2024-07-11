2c2421f7f0c85b26118f4eee74e86501dd6a1656
Katsushi OUGI <ougi@to-r.net>
2024-07-11 21:07:20 +0900

    Loading追加

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ function App() {
 
   const [output, setOutput] = useState<string | null>(null);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (!files || files.length === 0) return;
@@ -33,17 +35,28 @@ function App() {
   };
 
   const handleComplete = async () => {
+    setIsLoading(true);
     const blob = (await onGenerateCroppedImage()) as Blob;
     setOutput(URL.createObjectURL(blob));
+    setIsLoading(false);
   };
 
   return (
     <>
       <div>
-        <input type="file" ref={inputRef} onChange={handleChange} />
+        <input
+          type="file"
+          ref={inputRef}
+          onChange={handleChange}
+          disabled={isLoading}
+        />
         <div>
-          <button onClick={handleClear}>Clear</button>
-          <button onClick={handleComplete}>Complete</button>
+          <button onClick={handleClear} disabled={isLoading}>
+            Clear
+          </button>
+          <button onClick={handleComplete} disabled={isLoading}>
+            Complete
+          </button>
         </div>
         <div
           style={{
